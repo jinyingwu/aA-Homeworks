c685bdcb295e881dfb79d7eca138d58eb5f27043
jinying wu <jinyingwu@jinyings-MacBook-Air.local>
2018-08-24 00:26:09 -0700

    add #set and #get in Map class

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,17 +1,30 @@
-class map
+class Map
   def initialize
     @map = []
   end
 
   def set(key, value)
+    @map << [key, value]
   end
 
   def get(key)
+    @map.each do |el|
+      return el[1] if el[0] == key
+    end
+
+    nil
   end
 
   def delete(key)
   end
 
   def show
-  end 
+  end
 end
+
+map = Map.new
+map.set(1,10)
+map.set(2,20)
+map.set(3,30)
+
+p map.get(3)
