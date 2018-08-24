fb00a8681064bbfb1a2e3bafef22ebf7cf17b56d
jinying wu <jinyingwu@jinyings-MacBook-Air.local>
2018-08-24 00:03:48 -0700

    add #push #pop #peek on Stack class

--- W1D5/stack.rb
+++ W1D5/stack.rb
@@ -1,17 +1,27 @@
 class Stack
   def initialize
     # create ivar to store stack here!
+    @stack = []
   end
 
   def push(el)
     # adds an element to the stack
+    @stack << el
   end
 
   def pop
     # removes one element from the stack
+    @stack.pop
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
+    @stack.last
   end
-end 
+end
+
+sta = Stack.new
+sta.push(33)
+sta.push(66)
+p sta.peek
+p sta
