bae2ab37094780d6300c7c70a8612ee1ec60cf85
jinying wu <jinyingwu@jinyings-MacBook-Air.local>
2018-09-04 10:02:13 -0700

    add playwright class

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -19,6 +19,33 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    play = PlayDBConnection.instance.execute(<<-SQL, title)
+
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        title = ?
+    SQL
+    return nil unless play.length > 0
+  end
+
+  # def self.find_by_playwright(name)
+  #   playwright = Playwright.find_by_name(name)
+  #   play = PlayDBConnection.instance.execute(<<-SQL, name)
+  #     SELECT
+  #       *
+  #     FROM
+  #       playwright
+  #     WHERE
+  #       name = ?
+  #   SQL
+  #
+  #   return nil unless play.length > 0
+  # end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -49,3 +76,32 @@ class Play
     SQL
   end
 end
+
+class Playwright
+  attr_accessor :name, :birth_year
+  attr_reader :id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwright")
+    data.map {|datum| Playwright.new(datum)}
+  end
+
+  def self.find_by_name(name)
+    person = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwright
+      WHERE
+        name = ?
+    SQL
+    return nil unless person.length > 0
+
+    Playwright.new(person.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
